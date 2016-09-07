633342979536e498c421b3dc83cfcbffd3cb333b
kevin <kevinchou.c@gmail.com>
2016-09-07 12:50:56 +0800

    Load passed script

--- tools/blender/main.py
+++ tools/blender/main.py
@@ -6,16 +6,41 @@
 import bpy
 import os
 import argparse
+import json
+import sys
 
 parser = argparse.ArgumentParser(description='Process Generate args.')
 parser.add_argument('--gen', type=str, nargs='+',
-                    help='command begin generate')
+                    help='Command begin generate')
 parser.add_argument('--env', type=str, nargs='+',
-                    help='define the enviroment')
+                    help='Define the enviroment')
+
+passed_argv = sys.argv[(sys.argv.index("--")+1):]
 
 # Parse args
-args, unknown = parser.parse_known_args()
+args, unknown = parser.parse_known_args(passed_argv)
+
+# Get the working location
+file_path = os.getcwd()
+
+# Read package.json
+with open(os.path.join(file_path,'package.json')) as data_file:    
+	# parse package json
+    config_data = json.load(data_file)
+
+deps = config_data["deps"]
+
+deps_list = list()
+
+for dep in deps:
+	dep_name = list(dep.keys())[0]
+	dep_version = list(dep.values())[0]
+	dep_path = os.path.join(file_path,'luban_modules', dep_name)
+
 print(args)
+print(file_path)
+print(config_data)
+print(passed_argv)
 
 # def importFBX():
  
